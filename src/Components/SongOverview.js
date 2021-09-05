ae86ea71970d8af523122801a84f332b294e8363
Sander Meijer <sandertje1972@hotmail.com>
2021-09-05 21:27:31 +0200

    added sortering

--- src/Components/SongOverview.js
+++ src/Components/SongOverview.js
@@ -16,7 +16,7 @@ class SongOverview extends React.Component {
                 {id: 4, title: "Rumours", artist: "Timex Social Club", genre: "'80's Kneiters", stars: "☆☆☆☆☆"},
                 {id: 5, title: "Codo", artist: "Döf", genre: "Deutsche Schallplatten", stars: "☆☆☆"},
                 {id: 6, title: "Speeltijd", artist: "Niels Destadsbader", genre: "Vlaamse vrienden", stars: "☆☆☆☆"},
-                {id: 7, title: "Shame and scandal in the family", artist: "Shawn Elliott", genre: "Golden oldies", stars: "☆☆"},
+                {id: 7, title: "Hurricane", artist: "Bob Dylan", genre: "Golden oldies", stars: "☆☆"},
                 {id: 8, title: "Need you now", artist: "Lady Antebellum", genre: "21e eeuw", stars: "☆☆☆☆"},
                 {id: 9, title: "Where the wild roses grow", artist: "Nick Cave & Kylie Minogue", genre: "No-nonsense Nineties", stars: "☆"},
                 {id: 10, title: "Stirb nicht vor mir", artist: "Rammstein & Sharleen Spitari", genre: "Deutsche Schallplatten", stars: "☆☆"},
@@ -29,19 +29,26 @@ class SongOverview extends React.Component {
                 {id: 17, title: "I got 5 on it", artist: "Luniz", genre: "No-nonsense Nineties", stars: "☆☆☆"},
                 {id: 18, title: "Dodenrit", artist: "Drs.P", genre: "Nederlandstalig", stars: "☆☆☆☆☆"}
             ],
-            sort: ""
+            sort: "",
         }
     }
 
     render() {
-        const capitalize = (str) => {            
+        const capitalizeSong = (str) => {            
             return str.charAt(0).toUpperCase() + str.slice(1)
+        }
+        const capitalizeArtist = (str) => {
+            const words = str.split(" ")
+            for (let i=0; i<words.length; i++) {
+                words[i] = words[i][0].toUpperCase() + words[i].substr(1)                
+            }            
+            return words.join(" ")
         }
         const addNewSong = song => {
             const newSong = {
                 id: this.state.songs.length + 1,
-                title: capitalize(song.title),
-                artist: capitalize(song.artist), 
+                title: capitalizeSong(song.title),
+                artist: capitalizeArtist(song.artist), 
                 genre: song.genre, 
                 stars: song.stars
             }
@@ -50,28 +57,30 @@ class SongOverview extends React.Component {
         const emptySongs = () => {
             this.setState({songs: []})
         }
-        const sortSongs = () => {
-            switch(this.state.sort) {
+        const sortSongs = event => {
+            const selectedItem = event.target.value
+            console.log(selectedItem)
+            switch(selectedItem) {
                 case "TAZ":
-                    return this.setState({songs: this.state.songs.sort((a,b) => (a.title > b.title) ? 1 : -1)})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.title > b.title) ? 1 : -1)})
                     break
                 case "TZA":
-                    return this.setState({songs: this.state.songs.sort((a,b) => (a.title > b.title) ? -1 : 1)})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.title > b.title) ? -1 : 1)})
                     break
                 case "AAZ":
-                    return this.setState({songs: this.state.songs.sort((a,b) => (a.artist > b.artist ) ? 1 : -1)})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.artist > b.artist ) ? 1 : -1)})
                     break
                 case "AZA":
-                    return this.setState({songs: this.state.songs.sort((a,b) => (a.artist > b.artist ) ? -1 : 1)})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.artist > b.artist ) ? -1 : 1)})
                     break
                 case "SHL":
-                    return this.setState({songs: this.state.songs.sort((a,b) => (a.stars.length > b.stars.length ) ? 1 : -1)})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.stars.length > b.stars.length ) ? -1 : 1)})
                     break
                 case "SLH":
-                    return this.setState({songs: this.state.songs.sort((a,b) => (a.stars.length > b.stars.length ) ? -1 : 1)})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.stars.length > b.stars.length ) ? 1 : -1)})
                     break
                 default:
-                    return this.setState({songs: this.state.songs})
+                    this.setState({songs: this.state.songs.sort((a,b) => (a.id > b.id) ? 1 : -1)})
             }
         }
         return (
